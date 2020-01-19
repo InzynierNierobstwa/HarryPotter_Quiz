a9a0e056464ae88f55d5ef300c9ed926c9fe5b26
kamila <31627563+InzynierNierobstwa@users.noreply.github.com>
2020-01-19 23:04:11 +0100

    different idea

--- app.js
+++ app.js
@@ -1,112 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const btnStart = document.querySelector(".playTutorial__button--start");
-  const sectionPlace = document.querySelector(".playTutorial");
+  const sectionPlace = document.querySelector(".questionPlace");
 
-  //randomquestion
-  const mathRandom = function() {
-    return Math.floor(Math.random() * 8 + 1);
-  };
+  //btns
+  const lvlBtn = document.querySelectorAll(".lvlBundle__button");
+  const categoryBtn = document.querySelectorAll(".categoryBundle__button");
 
-  //xmlhttp
-  const loadQuestion = function() {
+  function loadJSON() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "/json/database.json", true);
 
     xhr.onload = function() {
-      if (this.status == 200) {
-        question = JSON.parse(this.responseText);
+      if (xhr.status == 200) {
+        let json = JSON.parse(xhr.responseText);
+        console.log(json);
+        return json;
       }
     };
     xhr.send();
-  };
+  }
 
-  //lvl panel
-  const lvlStart = function() {
-    const lvlBundle = document.createElement("div");
-    lvlBundle.classList.add("lvl__bundle");
-    sectionPlace.appendChild(lvlBundle);
+  loadJSON();
 
-    const lvlTable = ["easy", "medium", "hard", "mix"];
-    let elem;
-
-    lvlTable.forEach(function(el) {
-      elem = document.createElement("button");
-      elem.classList.add("lvl__button");
-      elem.innerHTML = el;
-      lvlBundle.appendChild(elem);
-
-      elem.addEventListener("click", function(e) {
-        e.preventDefault();
-        categoryStart();
-      });
-    });
-  };
-
-  //category panel
-  const categoryStart = function() {
-    const categoryBundle = document.createElement("div");
-    categoryBundle.classList.add("category__bundle");
-    sectionPlace.appendChild(categoryBundle);
-
-    const categoryTable = [
-      "Spells",
-      "Alchemy",
-      "Herbology",
-      "History",
-      "Law",
-      "Magical Creatures",
-      "Character",
-      "All"
-    ];
-
-    let elem;
-    categoryTable.forEach(function(el) {
-      elem = document.createElement("button");
-      elem.classList.add("category__button");
-      elem.innerHTML = el;
-      categoryBundle.appendChild(elem);
-
-      elem.addEventListener("click", function(e) {
-        e.preventDefault();
-        questionFrame(loadQuestion().innerText);
-      });
-    });
-  };
-
-  //grab info about lvl and category
-  const listener = {
-    lvl: function() {
-      document.addEventListener("click", function(event) {
-        if (event.target.classList.contains("lvl__button")) {
-          console.log(event.target.innerHTML);
-        }
-      });
-      return event.target.innerHTML;
-    },
-    category: function() {
-      document.addEventListener("click", function(event) {
-        if (event.target.classList.contains("category__button")) {
-          console.log(event.target.innerHTML);
-        }
-      });
-      return event.target.innerHTML;
-    }
-  };
-
-  //start
-  btnStart.addEventListener("click", function(e) {
-    e.preventDefault();
-    lvlStart();
-  });
-
-  listener.lvl();
-  listener.category();
-
-  let test = listener.lvl();
-  console.log(test);
-
-  //question frame creator
-  const questionFrame = function(question, answer) {
+  //tworzy ramke na pytania
+  function questionFrame(question, answer) {
     const quesFrame = document.createElement("div");
     quesFrame.classList.add("question__frame");
 
@@ -127,5 +43,53 @@ document.addEventListener("DOMContentLoaded", function() {
       quesAnswer.innerHTML = "1993";
       quesAnswerBundler.appendChild(quesAnswer);
     }
+  }
+
+  //obiekt ktory uniemozliwia ponowne klikniecie
+  function Disable(classId, name) {
+    this.name = name;
+    this.clearBtnStyle = function() {
+      classId.forEach(function(elem) {
+        elem.style.pointerEvents = "none";
+      });
+    };
+  }
+
+  const disableLvlBtn = new Disable(lvlBtn, "level");
+  const disableLvlCategory = new Disable(categoryBtn, "category");
+
+  //sprwadz czy tablica ma dwa argumenty
+  function showQuestion(arr) {
+    if (arr.length === 2) {
+      questionFrame();
+    }
+  }
+
+  let requirementArr = [];
+  //obiekt wprowadzajacy dane o lvl i kategorii
+  const checkRequirem = {
+    level: function() {
+      lvlBtn.forEach(function(elem) {
+        elem.addEventListener("click", function(e) {
+          requirementArr.push(e.target.innerText);
+          console.log(requirementArr);
+          disableLvlBtn.clearBtnStyle();
+          showQuestion(requirementArr);
+        });
+      });
+    },
+    category: function() {
+      categoryBtn.forEach(function(elem) {
+        elem.addEventListener("click", function(e) {
+          requirementArr.push(e.target.innerText);
+          console.log(requirementArr);
+          disableLvlCategory.clearBtnStyle();
+          showQuestion(requirementArr);
+        });
+      });
+    }
   };
+
+  checkRequirem.level();
+  checkRequirem.category();
 });
